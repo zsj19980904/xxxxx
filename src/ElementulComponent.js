3115d5b74f7f22448b03bf410ebf3ab1994eccfa
zsj19980904 <865326629@qq.com>
2022-07-17 11:13:01 +0800

    add README

---
+++ src/ElementulComponent.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+//按需引入
+import { Button} from 'element-ui';
+
+//应用ElementUI
+Vue.component(Button.name, Button);
---
+++ src/view/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h3>B呼</h3>
+		
+		<div></div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+	.div{
+		background:blue;
+		h3{
+			color: skyblue;
+			font-size: 50px;
+			text-align: center;
+			margin-top: 30px;
+		}
+	}
+</style>
